a0231d900701b7b6f41b30b5ce25270a68fe348f
Kagami Sascha Rosylight <saschanaz@outlook.com>
2013-06-22 11:31:22 +0000

    checkin

--- JPXRase/JPXRase/app.js
+++ JPXRase/JPXRase/app.js
@@ -19,16 +19,35 @@ var Greeter = (function () {
     return Greeter;
 })();
 
+var JXRChaser = (function () {
+    function JXRChaser() {
+    }
+    return JXRChaser;
+})();
+
 var JXRase = (function () {
     function JXRase() {
     }
+    JXRase.prototype.react = function () {
+        alert('reacted');
+    };
     return JXRase;
 })();
 
 window.onload = function () {
     var image = new Image();
-    image.onload = image.onerror = function () {
+    image.onload = function () {
+        if (image.height != 3 || image.width != 2)
+            startReaction();
     };
-    image.src = "data:image/vnd.ms-photo;base64,SUm8AQgAAAAJAAG8AQAQAAAAegAAAAK8BAABAAAAAAAAAAS8BAABAAAAAAAAAIC8BAABAAAAAgAAAIG8BAABAAAAAgAAAIK8CwABAAAAAADAQoO8CwABAAAAAADAQsC8BAABAAAAigAAAMG8BAABAAAAuQAAAAAAAAAkw91vA07+S7GFPXd2jckMV01QSE9UTwARRMBxAAEAAWAAoAAKAACgAAAAAQAAAAgAMP8ABEKAAAEAAAEBykUAAAAAAQI4GIGOEOIcaNEYRhtafaPkfgfuPzAAAAAAAAAAAAAAAAAAAAAAAAEDI/AcEzYgmbGE7Igl8IJfDCa4YTXCCRwgkcMJejCTwwk8MI88I8wiKgocpBP6QAmdCChzSCf0gBM7QUOUgn9IATO0FDlIJ/SAEzoQUOaQT+kAJnQgoc0gn9IATOg=";
+    image.onerror = function () {
+        startReaction();
+    };
+    image.src = "data:image/vnd.ms-photo;base64,SUm8AQgAAAAJAAG8AQAQAAAAegAAAAK8BAABAAAAAAAAAAS8BAABAAAAAAAAAIC8BAABAAAAAgAAAIG8BAABAAAAAwAAAIK8CwABAAAAAADAQoO8CwABAAAAAADAQsC8BAABAAAAigAAAMG8BAABAAAADgEAAAAAAAAkw91vA07+S7GFPXd2jckMV01QSE9UTwARRMBxAAEAAmAAoAAKAACgAAAAAQAAAAkAPv8ABEKAAAEAAAEByQ1Yf8AAAAEC+CFiBD4ggohx4eEAEYaNG1TNAiQC9xR+0RLkCyGAAABAMAALCApgSCe/8AAAAAAAAAAAAQMjN6DL0wTgiCRowm+GEBEEfCCSwwmmGEqhBEogj4QTUjCSQgl5wQ2CPqCiemEkSMJ8QQQUOaQT+kAJnaCiemEkSMJ8QVBRPTCSJGE+IIIKHNIJ/SAEzoQUOaQT+kAJnaCVUgksQgjTF0EqpBJYhBGmLoJVSCSyQRpy6CVUgksiCNMTsKHMwn9QhM7wocmE/pBCZ3hQ5MJ/SCEzvChyYT+oQmdA";
 };
+
+function startReaction() {
+    var jxrase = new JXRase();
+    jxrase.react();
+}
 //@ sourceMappingURL=app.js.map
