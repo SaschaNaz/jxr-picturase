0a691db1ff11bab55c74c48c714c5bbda7581b97
SaschaNaz_cp <SND\SaschaNaz_cp>
2013-06-25 14:47:28 +0000

    currently porting ReadContainer

--- JPXRase/JPXRase/app.js
+++ JPXRase/JPXRase/app.js
@@ -19,35 +19,121 @@ var Greeter = (function () {
     return Greeter;
 })();
 
-var JXRChaser = (function () {
-    function JXRChaser() {
+var JxrBindingDomain = (function () {
+    function JxrBindingDomain() {
     }
-    return JXRChaser;
+    return JxrBindingDomain;
 })();
 
-var JXRase = (function () {
-    function JXRase() {
+var ActiveSite = (function () {
+    function ActiveSite() {
     }
-    JXRase.prototype.react = function () {
-        alert('reacted');
+    ActiveSite.prototype.react = function (file) {
+        var JxrInvalidMessage = "This format is not a valid JPEG XR format for JXR Picturase.";
+        var JxrVersionTooHighMessage = "Current version of JXR Picturase doesn't support this version of JPEG XR.";
+
+        var stream = new ArrayedStream(file);
+
+        if (stream.readAsText(2) !== 'II')
+            throw JxrInvalidMessage;
+        var jxrId = stream.readAsUint16();
+        if ((0xFF & jxrId) != 0xBC)
+            throw JxrInvalidMessage;
+
+         {
+            var versionNumber = (0xFF00 & jxrId) >> 8;
+            if (versionNumber != 0 && versionNumber != 1)
+                throw JxrVersionTooHighMessage;
+        }
+
+        stream.seek(stream.readAsUint32());
     };
-    return JXRase;
+    return ActiveSite;
+})();
+
+var ArrayedStream = (function () {
+    function ArrayedStream(file) {
+        this.filearray = new Uint8Array(file);
+        this.index = 0;
+    }
+    ArrayedStream.prototype.readAsArrayBy = function (length) {
+        var indexbefore = this.index;
+        var indexafter = this.index + length;
+        if (indexafter <= this.filearray.length) {
+            this.index = indexafter;
+            return this.filearray.subarray(indexbefore, indexafter);
+        } else
+            throw "File reached to the end.";
+    };
+
+    ArrayedStream.prototype.readAsUint8 = function () {
+        if (this.index + 1 <= this.filearray.length) {
+            var uint8 = this.filearray[this.index];
+            this.index++;
+            return uint8;
+        } else
+            throw "File reached to the end.";
+    };
+
+    ArrayedStream.prototype.readAsUint16 = function () {
+        if (this.index + 2 <= this.filearray.length) {
+            var uint16 = this.filearray[this.index] + (this.filearray[this.index + 1] << 8);
+            this.index += 2;
+            return uint16;
+        } else
+            throw "File reached to the end.";
+    };
+
+    ArrayedStream.prototype.readAsUint32 = function () {
+        if (this.index + 4 <= this.filearray.length) {
+            var uint32 = this.filearray[this.index] + (this.filearray[this.index + 1] << 8) + (this.filearray[this.index + 2] << 16) + (this.filearray[this.index + 3] << 24);
+            this.index += 4;
+            return uint32;
+        } else
+            throw "File reached to the end.";
+    };
+
+    ArrayedStream.prototype.readAsText = function (length) {
+        return String.fromCharCode.apply(null, this.readAsArrayBy(length));
+    };
+
+    ArrayedStream.prototype.moveBy = function (length) {
+        var indexafter = this.index + length;
+        if (indexafter >= 0 && indexafter <= this.filearray.length)
+            this.index = indexafter; else
+            throw "The stream couldn't seek that position.";
+    };
+
+    ArrayedStream.prototype.seek = function (position) {
+        if (position >= 0 && position <= this.filearray.length)
+            this.index = position; else
+            throw "The stream couldn't seek that position.";
+    };
+    return ArrayedStream;
 })();
 
 window.onload = function () {
     var image = new Image();
-    image.onload = function () {
-        if (image.height != 3 || image.width != 2)
-            startReaction();
-    };
+
     image.onerror = function () {
         startReaction();
     };
     image.src = "data:image/vnd.ms-photo;base64,SUm8AQgAAAAJAAG8AQAQAAAAegAAAAK8BAABAAAAAAAAAAS8BAABAAAAAAAAAIC8BAABAAAAAgAAAIG8BAABAAAAAwAAAIK8CwABAAAAAADAQoO8CwABAAAAAADAQsC8BAABAAAAigAAAMG8BAABAAAADgEAAAAAAAAkw91vA07+S7GFPXd2jckMV01QSE9UTwARRMBxAAEAAmAAoAAKAACgAAAAAQAAAAkAPv8ABEKAAAEAAAEByQ1Yf8AAAAEC+CFiBD4ggohx4eEAEYaNG1TNAiQC9xR+0RLkCyGAAABAMAALCApgSCe/8AAAAAAAAAAAAQMjN6DL0wTgiCRowm+GEBEEfCCSwwmmGEqhBEogj4QTUjCSQgl5wQ2CPqCiemEkSMJ8QQQUOaQT+kAJnaCiemEkSMJ8QVBRPTCSJGE+IIIKHNIJ/SAEzoQUOaQT+kAJnaCVUgksQgjTF0EqpBJYhBGmLoJVSCSyQRpy6CVUgksiCNMTsKHMwn9QhM7wocmE/pBCZ3hQ5MJ/SCEzvChyYT+oQmdA";
+
+    startReaction();
 };
 
 function startReaction() {
-    var jxrase = new JXRase();
-    jxrase.react();
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "temp.txt", true);
+    xhr.responseType = "arraybuffer";
+
+    var arraybuffer;
+    xhr.onload = function () {
+        arraybuffer = xhr.response;
+
+        var jxrase = new ActiveSite();
+        jxrase.react(arraybuffer);
+    };
+    xhr.send();
 }
-//@ sourceMappingURL=app.js.map
--- JPXRase/JPXRase/app.ts
+++ JPXRase/JPXRase/app.ts
@@ -27,8 +27,30 @@ class JxrBindingDomain {
 
 class ActiveSite {
     react(file: ArrayBuffer) {
+        //porting ReadContainer method
 
-        alert('reacted');
+        var JxrInvalidMessage = "This format is not a valid JPEG XR format for JXR Picturase.";
+        var JxrVersionTooHighMessage = "Current version of JXR Picturase doesn't support this version of JPEG XR.";
+
+        var stream = new ArrayedStream(file);
+
+        //signiture
+        if (stream.readAsText(2) !== 'II')
+            throw JxrInvalidMessage;
+        var jxrId = stream.readAsUint16();
+        if ((0xFF & jxrId) != 0xBC)
+            throw JxrInvalidMessage;
+
+        //JXR version check - only 00 and 01 will be accepted
+        {
+            var versionNumber = (0xFF00 & jxrId) >> 8;
+            if (versionNumber != 0 && versionNumber != 1)
+                throw JxrVersionTooHighMessage;
+        }
+
+        //PFD - what is PFD? format decoder?
+        stream.seek(stream.readAsUint32());
+
     }
 }
 
@@ -37,34 +59,68 @@ class ArrayedStream {
     private filearray: Uint8Array;
 
     constructor(file: ArrayBuffer) {
-        var filearray = new Uint8Array(file);
+        this.filearray = new Uint8Array(file);
+        this.index = 0;
     }
 
     readAsArrayBy(length: number) {
-        if (this.index + length <= this.filearray.length)
-            return this.filearray.subarray(this.index, this.index + length);
+        var indexbefore = this.index;
+        var indexafter = this.index + length;
+        if (indexafter <= this.filearray.length) {
+            this.index = indexafter;
+            return this.filearray.subarray(indexbefore, indexafter);
+        }
         else
             throw "File reached to the end.";
     }
 
-    readAsByte() {
+    readAsUint8() {
         if (this.index + 1 <= this.filearray.length) {
+            var uint8 = this.filearray[this.index]
             this.index++;
-            return this.filearray[this.index];
+            return uint8;
         }
+        else
+            throw "File reached to the end.";
+    }
+
+    //little endian
+    readAsUint16() {
+        if (this.index + 2 <= this.filearray.length) {
+            var uint16 = this.filearray[this.index] + (this.filearray[this.index + 1] << 8);
+            this.index += 2;
+            return uint16;
+        }
+        else
+            throw "File reached to the end.";
+    }
+
+    //little endian
+    readAsUint32() {
+        if (this.index + 4 <= this.filearray.length) {
+            var uint32 = this.filearray[this.index] + (this.filearray[this.index + 1] << 8) + (this.filearray[this.index + 2] << 16) + (this.filearray[this.index + 3] << 24);
+            this.index += 4;
+            return uint32;
+        }
+        else
+            throw "File reached to the end.";
     }
 
     readAsText(length: number) {
-        if (this.index + length < this.filearray.length)
-            return this.filearray.subarray(this.index, this.index + length);
-        else
-            throw "File reached to the end.";
+        return String.fromCharCode.apply(null, this.readAsArrayBy(length));
     }
 
-    moveBy(position: number) {
-        var indexafter = this.index + position;
-        if (indexafter >= 0)
+    moveBy(length: number) {
+        var indexafter = this.index + length;
+        if (indexafter >= 0 && indexafter <= this.filearray.length)
             this.index = indexafter;
+        else
+            throw "The stream couldn't seek that position.";
+    }
+
+    seek(position: number) {
+        if (position >= 0 && position <= this.filearray.length)
+            this.index = position;
         else
             throw "The stream couldn't seek that position.";
     }
@@ -75,9 +131,21 @@ window.onload = () => {
     //image.onload = () => { if (image.height != 3 || image.width != 2) startReaction(); };
     image.onerror = () => { startReaction(); };
     image.src = "data:image/vnd.ms-photo;base64,SUm8AQgAAAAJAAG8AQAQAAAAegAAAAK8BAABAAAAAAAAAAS8BAABAAAAAAAAAIC8BAABAAAAAgAAAIG8BAABAAAAAwAAAIK8CwABAAAAAADAQoO8CwABAAAAAADAQsC8BAABAAAAigAAAMG8BAABAAAADgEAAAAAAAAkw91vA07+S7GFPXd2jckMV01QSE9UTwARRMBxAAEAAmAAoAAKAACgAAAAAQAAAAkAPv8ABEKAAAEAAAEByQ1Yf8AAAAEC+CFiBD4ggohx4eEAEYaNG1TNAiQC9xR+0RLkCyGAAABAMAALCApgSCe/8AAAAAAAAAAAAQMjN6DL0wTgiCRowm+GEBEEfCCSwwmmGEqhBEogj4QTUjCSQgl5wQ2CPqCiemEkSMJ8QQQUOaQT+kAJnaCiemEkSMJ8QVBRPTCSJGE+IIIKHNIJ/SAEzoQUOaQT+kAJnaCVUgksQgjTF0EqpBJYhBGmLoJVSCSyQRpy6CVUgksiCNMTsKHMwn9QhM7wocmE/pBCZ3hQ5MJ/SCEzvChyYT+oQmdA";
+
+    startReaction();
 };
 
 function startReaction() {
-    var jxrase = new ActiveSite();
-    jxrase.react;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "temp.txt", true);
+    xhr.responseType = "arraybuffer";
+
+    var arraybuffer: ArrayBuffer;
+    xhr.onload = () => {
+        arraybuffer = xhr.response;
+        
+        var jxrase = new ActiveSite();
+        jxrase.react(arraybuffer);
+    }
+    xhr.send();
 }
