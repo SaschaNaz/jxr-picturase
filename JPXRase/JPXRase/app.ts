4299186e53dbf2399bc79cdac6133990fbf7fb61
Kagami Sascha Rosylight <saschanaz@outlook.com>
2013-06-26 05:06:11 +0000

    all the tags ready for port

--- JPXRase/JPXRase/app.ts
+++ JPXRase/JPXRase/app.ts
@@ -1,26 +1,3 @@
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
 class JxrBindingDomain {
 
 }
@@ -50,7 +27,160 @@ class ActiveSite {
 
         //PFD - what is PFD? format decoder?
         stream.seek(stream.readAsUint32());
+        {
+            var pfdEntries = stream.readAsUint16();
+            if (pfdEntries == 0 && pfdEntries == 0xFFFF)
+                throw JxrInvalidMessage;
+        }
+    }
 
+    //ported version of ParsePFD
+    private parsePFDEntries(stream: ArrayedStream, pfdEntries: number) {
+        for (var i = 0; i < pfdEntries; i++) {
+            this.parsePFD(
+                stream,
+                stream.readAsUint16(),
+                stream.readAsUint16(),
+                stream.readAsUint32(),
+                stream.readAsUint32());
+        }
+    }
+
+    //ported version of ParsePFDEntry
+    private parsePFD(stream: ArrayedStream, tag: number, type: number, count: number, value: number) {
+        switch (tag) {
+            case 0xBC01: //pixel format tag
+                {
+                    break;
+                }
+            case 0xBC02: //transformation tag
+                {
+                    break;
+                }
+            case 0xBC80: //image width tag
+                {
+                    break;
+                }
+            case 0xBC81: //image height tag
+                {
+                    break;
+                }
+            case 0xBCC0: //image offset tag
+                {
+                    break;
+                }
+            case 0xBCC1: //image byte count tag
+                {
+                    break;
+                }
+            case 0xBCC2: //alpha offset tag
+                {
+                    break;
+                }
+            case 0xBCC3: // alpha byte count tag
+                {
+                    break;
+                }
+            case 0xBC82: // width resolution tag
+                {
+                    break;
+                }
+            case 0xBC83: // height resolution tag
+                {
+                    break;
+                }
+            case 0x8773: // ICC profile tag - same as TIFF
+                {
+                    break;
+                }
+            case 0x02BC: // XMP metadata tag
+                {
+                    break;
+                }
+            case 0x8769: // EXIF metadata tag
+                {
+                    break;
+                }
+            case 0x8825: // GPS info metadata tag
+                {
+                    break;
+                }
+            case 0x83BB: // IPTC-NAA metadata tag
+                {
+                    break;
+                }
+            case 0x8649: // Photoshop metadata tag
+                {
+                    break;
+                }
+            case 0xBC03: // compression tag
+            case 0xBC04: // image type tag
+            case 0xBCC4: // (discarded) image data tag
+            case 0xBCC5: // (discarded) alpha data tag
+                break;
+            
+            //descriptive metadata
+            case 0x010E: // image description tag
+                {
+                    break;
+                }
+            case 0x010F: // camera manufacturer tag
+                {
+                    break;
+                }
+            case 0x0110: // camera model name tag
+                {
+                    break;
+                }
+            case 0x0131: // software tag
+                {
+                    break;
+                }
+            case 0x0132: // date and time tag
+                {
+                    break;
+                }
+            case 0x013B: // artist tag
+                {
+                    break;
+                }
+            case 0x8298: // copyright tag
+                {
+                    break;
+                }
+            case 0x4746: // rating stars tag
+                {
+                    break;
+                }
+            case 0x4749: // rating value tag
+                {
+                    break;
+                }
+            case 0x9C9B: // caption tag
+                {
+                    break;
+                }
+            case 0x010D: // document name tag
+                {
+                    break;
+                }
+            case 0x011D: // page name tag
+                {
+                    break;
+                }
+            case 0x0129: // page number tag
+                {
+                    break;
+                }
+            case 0x013c: // host computer tag
+                {
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
     }
 }
 
@@ -123,6 +253,10 @@ class ArrayedStream {
             this.index = position;
         else
             throw "The stream couldn't seek that position.";
+    }
+
+    getCurrentPosition() {
+        return this.index;
     }
 }
 
