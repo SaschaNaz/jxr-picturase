dd6164143fb98ce0cbfe3a725f2dc854bb8ae846
SaschaNaz_cp <SND\SaschaNaz_cp>
2013-08-31 15:02:16 +0000

    last in vacation

--- JPXRase/JPXRase/SubstrateComponents/imageplaneheader.ts
+++ JPXRase/JPXRase/SubstrateComponents/imageplaneheader.ts
@@ -5,16 +5,35 @@
 ///<reference path="errorobjectprovider.ts"/>
 module JxrPicturase.SubstrateComponents {
     export class ImagePlaneHeader {
+        internalColorFormat: InternalColorFormat;
+        willBeScaled: boolean;
+
+        chromaCenteringX = 0;
+
         static Parse(imageSubstream: ArrayedStream, imageHeader: ImageHeader, isAlphaPlane: boolean) {
+            var imagePlaneHeader = new ImagePlaneHeader();
             var bitstream = new ArrayedBitStream(imageSubstream);
 
-            var internalColorFormat: InternalColorFormat = bitstream.readBits(3);
-            if (!InternalColorFormat[internalColorFormat])
+            imagePlaneHeader.internalColorFormat = bitstream.readBits(3);
+            if (!InternalColorFormat[imagePlaneHeader.internalColorFormat])
                 throw new Error(JxrErrorMessage.getUnsupportedPropertyMessage("INTERNAL_CLR_FMT", "IMAGE_PLANE_HEADER"));
-            var willBeScaled = (bitstream.readBits(1) == 1);
+            imagePlaneHeader.willBeScaled = (bitstream.readBits(1) == 1);
             var bandsPresent = FrequencyBandsPresence.getBandsPresence(bitstream.readBits(4));
 
-            
+            if (imagePlaneHeader.internalColorFormat == InternalColorFormat.Yuv444
+                || imagePlaneHeader.internalColorFormat == InternalColorFormat.Yuv420
+                || imagePlaneHeader.internalColorFormat == InternalColorFormat.Yuv422) {
+
+                if (imagePlaneHeader.internalColorFormat == InternalColorFormat.Yuv420
+                    || imagePlaneHeader.internalColorFormat == InternalColorFormat.Yuv422) {
+                    if (bitstream.readBits(1) != 0)
+                        throw new Error(JxrErrorMessage.getUnsupportedPropertyMessage("RESERVED_E", "IMAGE_HEADER"));
+
+                    
+                }
+
+            }
+
         }
 
         getComponentCount() {
